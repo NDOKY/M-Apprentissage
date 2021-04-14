19fc919ffde222bdd615fe3aa08679be4eafe4ad
Ndoky <scorpiondoky@gmail.com>
2021-04-14 11:10:51 -0300

    commit du background de login

--- app/src/main/java/com/example/m_apprentissage/MainActivity.java
+++ app/src/main/java/com/example/m_apprentissage/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_login);
     }
 }
