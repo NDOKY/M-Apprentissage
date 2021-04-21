9c544460dfa3dfcb1a0a8bb8bcd84a01e1c57e50
Idric Kouemene <kouemenechristian@gmail.com>
2021-04-21 11:12:11 -0300

    Ajout du videoActivity.java

--- app/src/main/java/com/example/m_apprentissage/videoGalleryActivity.java
+++ app/src/main/java/com/example/m_apprentissage/videoGalleryActivity.java
@@ -1,29 +1,20 @@
 package com.example.m_apprentissage;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.Toast;
-import java.util.ArrayList;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class videoGalleryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videogallery);
-       // String video = "https://www.youtube.com/embed/BwqbJf-yayQ";
         final ListView list = findViewById(R.id.list);
         // String video = "";
         ArrayList<String> arrayList = new ArrayList<>();
@@ -38,12 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
         list.setOnItemClickListener((parent, view, position, id) -> {
             String clickedItem = (String) list.getItemAtPosition(position);
-            Toast.makeText(MainActivity.this, clickedItem, Toast.LENGTH_LONG).show();
+            Toast.makeText(videoGalleryActivity.this, clickedItem, Toast.LENGTH_LONG).show();
         });
-
-
-
-
-
     }
 }
+
