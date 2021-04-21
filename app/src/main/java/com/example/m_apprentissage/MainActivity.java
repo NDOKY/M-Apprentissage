3ecfe16e70edf2143e91347f1e4102dc010c66d9
Ndoky <scorpiondoky@gmail.com>
2021-04-21 10:53:12 -0300

    Liaison de l'application a Firebase et gestion du login

--- app/src/main/java/com/example/m_apprentissage/MainActivity.java
+++ app/src/main/java/com/example/m_apprentissage/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.m_apprentissage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*String http = "<iframe src=\"https://yvesndoky.h5p.com/content/1291302058534816898/embed\" width=\"1088\" height=\"637\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\" allow=\"geolocation *; microphone *; camera *; midi *; encrypted-media *\"></iframe><script src=\"https://yvesndoky.h5p.com/js/h5p-resizer.js\" charset=\"UTF-8\"></script>";
 
+        setContentView(R.layout.videoframe);
+        WebView webView = (WebView)findViewById(R.id.web);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadData(http, "text/html",null);*/
     }
 }
