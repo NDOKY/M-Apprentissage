dea5e5ed56b6043728ce9362864758052b4f0402
Idric Kouemene <kouemenechristian@gmail.com>
2021-04-21 11:12:11 -0300

    Ajout du videoActivity.java

---
+++ app/src/main/java/com/example/m_apprentissage/videoGalleryActivity.java
@@ -0,0 +1,36 @@
+package com.example.m_apprentissage;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class videoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videogallery);
+        final ListView list = findViewById(R.id.list);
+        // String video = "";
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("JAVA");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(arrayAdapter);
+
+        list.setOnItemClickListener((parent, view, position, id) -> {
+            String clickedItem = (String) list.getItemAtPosition(position);
+            Toast.makeText(videoGalleryActivity.this, clickedItem, Toast.LENGTH_LONG).show();
+        });
+    }
+}
+
